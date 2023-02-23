fd6f54f815ee3d1f8431c6da62aafff08536201e
Optimistola <yakubshakirudeenolaide2018@gmail.com>
2023-02-22 21:44:53 -0800

    updated

--- src/Components/Utils/Add.jsx
+++
@@ -1,9 +0,0 @@
-
-export const CartAdd=({image, price})=>{
-    return(
-        <div className='absolute top-10 center py-5 px-5 rounded-md hidden'>
-            <img src={image}/>
-            <p>{price}</p>
-        </div>
-    )
-}
