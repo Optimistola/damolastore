fe3d5bf850b1862c28c8c9ab1ddd10b3bbf127a6
Optimistola <yakubshakirudeenolaide2018@gmail.com>
2023-02-22 21:50:44 -0800

    updated

--- src/Components/Nav/Header.jsx
+++ src/Components/Nav/Header.jsx
@@ -17,7 +17,7 @@ return(
         <div className='md:flex  justify-between items-center font-secondary text-bold mt-2 capitalize bg-gray-900 text-white py-4'>
         <ul className=" ml-5 flex flex-wrap justify-betwen gap-3 md:gap-3 items-center contents-center">
                 {category.map((item)=>
-                 <li className='bg-gray-500  w-[30%] lg:w-[90px] md:w-[90px] hover:bg-gray-700 rounded-md lg:text-md text-center text-white'>
+                 <li className='bg-gray-500  w-[30%] lg:w-[90px] md:w-[90px] hover:bg-gray-700 rounded-md text-sm text-center text-white'>
                      <Link to={`category/${item.category}`}>{item.category}</Link>
                 </li>
                  )}
@@ -25,14 +25,14 @@ return(
             <div className='flex content-center items-center ml-5 md:mr-5 mt-3 md:mt-3 lg:mt-0'>
             </div>
             <div className='gap-3 flex  justify-center md:mr-20'>
-            <BiUser size={20}/>
-            <Link to='/cart'><AiOutlineShoppingCart size={20}/>
-            <span className='relative flex content-center text-sm items-center justify-center text-white -top-5 left-3  w-[15px] h-[15px] bg-red-600 rounded-full'>
+            <BiUser size={25}/>
+            <Link to='/cart'><AiOutlineShoppingCart size={25}/>
+            <span className='relative flex content-center text-sm items-center justify-center text-white -top-8 left-3  w-[15px] h-[15px] bg-red-600 rounded-full'>
                 {cartLength}
                 </span>
             </Link>
-            <Link to='/wish'><BiShoppingBag size={20}/>
-            <span className='relative flex content-center text-sm items-center justify-center text-white -top-5 left-3  w-[15px] h-[15px] bg-red-600 rounded-full'>
+            <Link to='/wish'><BiShoppingBag size={25}/>
+            <span className='relative flex content-center text-sm items-center justify-center text-white -top-8 left-3  w-[15px] h-[15px] bg-red-600 rounded-full'>
                 {wishLength}
                 </span>
             </Link>
