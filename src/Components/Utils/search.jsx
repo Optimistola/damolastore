7374f6e038c488be471085a53f0f7b39c4b9736a
Optimistola <yakubshakirudeenolaide2018@gmail.com>
2023-03-03 23:27:42 -0800

    updated

--- src/Components/Utils/search.jsx
+++ src/Components/Utils/search.jsx
@@ -2,11 +2,11 @@ import {IoSearch} from 'react-icons/io5';
 import { useState } from 'react';
  export const Search=({cat, action, action2})=>{ 
      return(
-         <div className='flex justify-center text-green-600 rounded-md ml-5 md:ml-10 border border-gray-400 w-[230px] '>
-             <input type='search' placeholder={`search ${cat}...`}
+         <div className=' p-5 flex '>
+             <input type='text' placeholder={`search ${cat}...`}
              onChange={action2}
-             className=' outline-none w-[200px]'/>
-             <button class='bg-red-600 px-2 py-1 text-white w-[30px]'><IoSearch size={20} onClick={action}/></button>
+             className=' outline-none w-[200px] border rounded-md border-gray-600 z-50'/>
+             <button ><IoSearch size={20}/></button>
          </div>
      )
  }
